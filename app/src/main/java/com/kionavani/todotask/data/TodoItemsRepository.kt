dcd03a6a140d760da7b74a40559a763b4817293e
kobay9iq <kby9iq@gmail.com>
2024-06-29 01:01:16 +0300

    maked async work with flow in repo

--- app/src/main/java/com/kionavani/todotask/data/TodoItemsRepository.kt
+++ app/src/main/java/com/kionavani/todotask/data/TodoItemsRepository.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 class TodoItemsRepository {
@@ -13,108 +14,113 @@ class TodoItemsRepository {
 
     private val repositoryScope = CoroutineScope(Dispatchers.IO)
 
-
     init {
         val currentTime = System.currentTimeMillis()
-        _todoItems.value = mutableListOf(
-            ToDoItem(
-                id = "1",
-                taskDescription = "Закончить проект",
-                isCompleted = false,
-                importance = Importance.HIGH,
-                creatingDate = currentTime,
-                deadlineDate = currentTime + 86400000 // плюс 1 день в миллисекундах
-            ),
-            ToDoItem(
-                id = "2",
-                taskDescription = "Сходить в магазин",
-                isCompleted = false,
-                importance = Importance.REGULAR,
-                creatingDate = currentTime,
-                deadlineDate = currentTime + 2 * 86400000 // плюс 2 дня в миллисекундах
-            ),
-            ToDoItem(
-                id = "3",
-                taskDescription = "Прочитать книгу",
-                isCompleted = true,
-                importance = Importance.LOW,
-                creatingDate = currentTime,
-                deadlineDate = currentTime + 3 * 86400000 // плюс 3 дня в миллисекундах
-            ),
-            ToDoItem(
-                id = "4",
-                taskDescription = "Посмотреть книгу, а потом написать " +
-                        "для нее невероятную рецензию на мой любимый кинопоиск. " +
-                        "Кстати, надеюсь это не будет багом из-за слишком длинной строки",
-                isCompleted = false,
-                importance = Importance.HIGH,
-                creatingDate = currentTime
-            ),
-            ToDoItem(
-                id = "5",
-                taskDescription = "Сгонять на пересдачу по матлогике",
-                isCompleted = false,
-                importance = Importance.LOW,
-                creatingDate = currentTime - 2 * 86400000, // минус 2 дня в миллисекундах
-                changingDate = currentTime - 86400000 // минус 1 день в миллисекундах
-            ),
-            ToDoItem(
-                id = "6",
-                taskDescription = "Сделать невероятно важное дело",
-                isCompleted = false,
-                importance = Importance.LOW,
-                creatingDate = currentTime - 2 * 86400000 // минус 2 дня в миллисекундах
-            ),
-            ToDoItem(
-                id = "7",
-                taskDescription = "Ничего не делать много времени",
-                isCompleted = true,
-                importance = Importance.HIGH,
-                creatingDate = currentTime
-            ),
-            ToDoItem(
-                id = "8",
-                taskDescription = "Спросить у ментора, что он ел на обед",
-                isCompleted = false,
-                importance = Importance.REGULAR,
-                creatingDate = currentTime - 86400000, // минус 1 день в миллисекундах
-                deadlineDate = currentTime - 7200000 // минус 2 часа в миллисекундах
-            ),
-            ToDoItem(
-                id = "9",
-                taskDescription = "Посмотреть лекцию по компоузу третий раз",
-                isCompleted = false,
-                importance = Importance.REGULAR,
-                creatingDate = currentTime - 4 * 86400000, // минус 4 дня в миллисекундах
-                deadlineDate = currentTime + 15 * 86400000 // плюс 15 дней в миллисекундах
-            ),
-            ToDoItem(
-                id = "10",
-                taskDescription = "Помыть воду из пятилитровки",
-                isCompleted = false,
-                importance = Importance.HIGH,
-                creatingDate = currentTime - 4 * 86400000, // минус 4 дня в миллисекундах
-                changingDate = currentTime - 3600000 // минус 1 час в миллисекундах
-            ),
-            ToDoItem(
-                id = "11",
-                taskDescription = "Создать 11 глупых тасков для теста приложения",
-                isCompleted = false,
-                importance = Importance.HIGH,
-                creatingDate = currentTime
-            ),
-        )
+        repositoryScope.launch {
+            _todoItems.emit(
+                listOf(
+                    ToDoItem(
+                        id = "1",
+                        taskDescription = "Закончить проект",
+                        isCompleted = false,
+                        importance = Importance.HIGH,
+                        creatingDate = currentTime,
+                        deadlineDate = currentTime + 86400000 // плюс 1 день в миллисекундах
+                    ),
+                    ToDoItem(
+                        id = "2",
+                        taskDescription = "Сходить в магазин",
+                        isCompleted = false,
+                        importance = Importance.REGULAR,
+                        creatingDate = currentTime,
+                        deadlineDate = currentTime + 2 * 86400000 // плюс 2 дня в миллисекундах
+                    ),
+                    ToDoItem(
+                        id = "3",
+                        taskDescription = "Прочитать книгу",
+                        isCompleted = true,
+                        importance = Importance.LOW,
+                        creatingDate = currentTime,
+                        deadlineDate = currentTime + 3 * 86400000 // плюс 3 дня в миллисекундах
+                    ),
+                    ToDoItem(
+                        id = "4",
+                        taskDescription = "Посмотреть книгу, а потом написать " +
+                                "для нее невероятную рецензию на мой любимый кинопоиск. " +
+                                "Кстати, надеюсь это не будет багом из-за слишком длинной строки",
+                        isCompleted = false,
+                        importance = Importance.HIGH,
+                        creatingDate = currentTime
+                    ),
+                    ToDoItem(
+                        id = "5",
+                        taskDescription = "Сгонять на пересдачу по матлогике",
+                        isCompleted = false,
+                        importance = Importance.LOW,
+                        creatingDate = currentTime - 2 * 86400000, // минус 2 дня в миллисекундах
+                        changingDate = currentTime - 86400000 // минус 1 день в миллисекундах
+                    ),
+                    ToDoItem(
+                        id = "6",
+                        taskDescription = "Сделать невероятно важное дело",
+                        isCompleted = false,
+                        importance = Importance.LOW,
+                        creatingDate = currentTime - 2 * 86400000 // минус 2 дня в миллисекундах
+                    ),
+                    ToDoItem(
+                        id = "7",
+                        taskDescription = "Ничего не делать много времени",
+                        isCompleted = true,
+                        importance = Importance.HIGH,
+                        creatingDate = currentTime
+                    ),
+                    ToDoItem(
+                        id = "8",
+                        taskDescription = "Спросить у ментора, что он ел на обед",
+                        isCompleted = false,
+                        importance = Importance.REGULAR,
+                        creatingDate = currentTime - 86400000, // минус 1 день в миллисекундах
+                        deadlineDate = currentTime - 7200000 // минус 2 часа в миллисекундах
+                    ),
+                    ToDoItem(
+                        id = "9",
+                        taskDescription = "Посмотреть лекцию по компоузу третий раз",
+                        isCompleted = false,
+                        importance = Importance.REGULAR,
+                        creatingDate = currentTime - 4 * 86400000, // минус 4 дня в миллисекундах
+                        deadlineDate = currentTime + 15 * 86400000 // плюс 15 дней в миллисекундах
+                    ),
+                    ToDoItem(
+                        id = "10",
+                        taskDescription = "Помыть воду из пятилитровки",
+                        isCompleted = false,
+                        importance = Importance.HIGH,
+                        creatingDate = currentTime - 4 * 86400000, // минус 4 дня в миллисекундах
+                        changingDate = currentTime - 3600000 // минус 1 час в миллисекундах
+                    ),
+                    ToDoItem(
+                        id = "11",
+                        taskDescription = "Создать 11 глупых тасков для теста приложения",
+                        isCompleted = false,
+                        importance = Importance.HIGH,
+                        creatingDate = currentTime
+                    ),
+                )
+            )
+        }
     }
 
     fun addTodoItem(item: ToDoItem) {
         repositoryScope.launch {
-            _todoItems.value += item
+            val currentList = _todoItems.first()
+            _todoItems.emit(currentList + item)
         }
     }
 
     fun updateTodoItem(newItem: ToDoItem) {
         repositoryScope.launch {
-            val itemToCompare = _todoItems.value.find { it.id == newItem.id }
+            val currentList = _todoItems.first()
+            val itemToCompare = currentList.find { it.id == newItem.id }
             if (itemToCompare != null) {
                 val itemToAdd = itemToCompare.copy(
                     taskDescription = newItem.taskDescription,
@@ -122,24 +128,26 @@ class TodoItemsRepository {
                     deadlineDate = newItem.deadlineDate,
                     changingDate = newItem.changingDate
                 )
-                _todoItems.value = _todoItems.value.map {
+                _todoItems.emit(currentList.map {
                     if (it.id == itemToAdd.id) itemToAdd else it
-                }
+                })
             }
         }
     }
 
     fun deleteTodoItem(itemId: String) {
         repositoryScope.launch {
-            _todoItems.value = _todoItems.value.filter { it.id != itemId }
+            val currentList = _todoItems.first()
+            _todoItems.emit(currentList.filter { it.id != itemId })
         }
     }
 
     fun toggleTaskCompletion(itemId: String, isCompleted: Boolean) {
         repositoryScope.launch {
-            _todoItems.value = _todoItems.value.map { item ->
+            val currentList = _todoItems.first()
+            _todoItems.emit(currentList.map { item ->
                 if (item.id == itemId) item.copy(isCompleted = isCompleted) else item
-            }
+            })
         }
     }
 
