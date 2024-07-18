44ba235b7ca2e97d7049e9bf0526e196b5a0a7d8
kobay9iq <kby9iq@gmail.com>
2024-07-18 03:03:15 +0300

    another one bugfix

--- app/src/main/java/com/kionavani/todotask/data/remote/Endpoints.kt
+++ app/src/main/java/com/kionavani/todotask/data/remote/Endpoints.kt
@@ -6,8 +6,8 @@ import com.kionavani.todotask.BuildConfig
  * Объект с константными эндпоинтами для работы с сетью
  */
 object Endpoints {
-    private val BASE_URL = BuildConfig.BASE_URL
-    val LIST = "$BASE_URL/list"
+    private const val BASE_URL = BuildConfig.BASE_URL
+    const val LIST = "$BASE_URL/list"
 
     fun getListOneUrl(id: String): String {
         return "$BASE_URL/list/$id"
