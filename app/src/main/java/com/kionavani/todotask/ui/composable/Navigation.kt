52e3c8f89ed67fcbdd4a27e57010600ed46b31b7
kobay9iq <kby9iq@gmail.com>
2024-07-20 03:17:30 +0300

    animation added

--- app/src/main/java/com/kionavani/todotask/ui/composable/Navigation.kt
+++ app/src/main/java/com/kionavani/todotask/ui/composable/Navigation.kt
@@ -1,6 +1,18 @@
 package com.kionavani.todotask.ui.composable
 
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -10,6 +22,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import com.kionavani.todotask.ui.divkit.ViewFactoryInt
+import com.kionavani.todotask.ui.theme.ToDoTaskTheme
 import com.kionavani.todotask.ui.viewmodels.AddTaskViewModel
 import com.kionavani.todotask.ui.viewmodels.MainScreenViewModel
 import com.kionavani.todotask.ui.viewmodels.SettingsViewModel
@@ -20,53 +33,85 @@ import kotlinx.serialization.Serializable
  */
 @Composable
 fun SetupUI(
-    viewModelFactory: ViewModelProvider.Factory,
-    aboutViewFactory: ViewFactoryInt
+    viewModelFactory: ViewModelProvider.Factory, aboutViewFactory: ViewFactoryInt
 ) {
     val navController = rememberNavController()
     aboutViewFactory.navController = navController
 
-    NavHost(
-        navController = navController,
-        startDestination = MainScreenNav
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(ToDoTaskTheme.colorScheme.backPrimary)
     ) {
-        composable<MainScreenNav> {
-            val viewModel = viewModel(
-                modelClass = MainScreenViewModel::class.java,
-                factory = viewModelFactory
-            )
+        NavHost(
+            navController = navController, startDestination = MainScreenNav
+        ) {
+            composable<MainScreenNav>(
+                enterTransition = { enterTransition() },
+                exitTransition = { exitTransition() },
+                popEnterTransition = { enterTransition() },
+                popExitTransition = { exitTransition() }
+            ) {
+                val viewModel = viewModel(
+                    modelClass = MainScreenViewModel::class.java, factory = viewModelFactory
+                )
 
-            val navigateToAdd =
-                { itemId: String? -> navController.navigate(AddTaskScreenNav(itemId)) }
-            val navigateToSettings = { navController.navigate(SettingsScreenNav) }
+                val navigateToAdd =
+                    { itemId: String? -> navController.navigate(AddTaskScreenNav(itemId)) }
+                val navigateToSettings = { navController.navigate(SettingsScreenNav) }
 
-            MainScreen(viewModel, navigateToAdd, navigateToSettings)
-        }
-        composable<AddTaskScreenNav> { backStackEntry ->
-            val itemID = backStackEntry.toRoute<AddTaskScreenNav>().itemID
-            val viewModel = viewModel(
-                modelClass = AddTaskViewModel::class.java,
-                factory = viewModelFactory
-            )
-            val navigate = { navController.popBackStack() }
+                MainScreen(viewModel, navigateToAdd, navigateToSettings)
+            }
+            composable<AddTaskScreenNav>(
+                enterTransition = { enterTransition() },
+                exitTransition = { exitTransition() },
+                popEnterTransition = { enterTransition() },
+                popExitTransition = { exitTransition() }
+            ) { backStackEntry ->
+                val itemID = backStackEntry.toRoute<AddTaskScreenNav>().itemID
+                val viewModel = viewModel(
+                    modelClass = AddTaskViewModel::class.java, factory = viewModelFactory
+                )
+                val navigate = { navController.popBackStack() }
 
-            AddTaskScreen(viewModel, itemID, navigate)
-        }
-        composable<SettingsScreenNav> {
-            val viewModel = viewModel(
-                modelClass = SettingsViewModel::class.java,
-                factory = viewModelFactory
-            )
+                AddTaskScreen(viewModel, itemID, navigate)
+            }
+            composable<SettingsScreenNav>(
+                enterTransition = { enterTransition() },
+                exitTransition = { exitTransition() },
+                popEnterTransition = { enterTransition() },
+                popExitTransition = { exitTransition() }
+            ) {
+                val viewModel = viewModel(
+                    modelClass = SettingsViewModel::class.java, factory = viewModelFactory
+                )
 
-            val navigateBack = { navController.popBackStack() }
-            val navigateToInfo = { navController.navigate(AboutInfoNav) }
-            SettingsScreen(viewModel, navigateBack, navigateToInfo)
-        }
-        composable<AboutInfoNav> {
-            AboutInfoScreen(aboutViewFactory)
+                val navigateBack = { navController.popBackStack() }
+                val navigateToInfo = { navController.navigate(AboutInfoNav) }
+                SettingsScreen(viewModel, navigateBack, navigateToInfo)
+            }
+            composable<AboutInfoNav>(
+                enterTransition = { enterTransition() },
+                exitTransition = { exitTransition() },
+                popEnterTransition = { enterTransition() },
+                popExitTransition = { exitTransition() }
+            ) {
+                AboutInfoScreen(aboutViewFactory)
+            }
         }
     }
+}
 
+fun enterTransition(): EnterTransition {
+    return slideInHorizontally(
+        initialOffsetX = { 1000 }, animationSpec = tween(700)
+    ) + fadeIn(animationSpec = tween(700))
+}
+
+fun exitTransition(): ExitTransition {
+    return slideOutHorizontally(
+        targetOffsetX = { -1000 }, animationSpec = tween(700)
+    ) + fadeOut(animationSpec = tween(700))
 }
 
 /**
